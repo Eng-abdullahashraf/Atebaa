48cd4c6d5a8033b435dff73b3480dd10376eab61
sekoo <abdullah.ashraf.r@gmail.com>
2024-01-16 21:16:03 +0200

    latestcom

--- lib/constant/addvertisingmodels.dart
+++ lib/constant/addvertisingmodels.dart
@@ -15,6 +15,8 @@ class AdvertisingModel{
 List<dynamic> addvertingmodel=[
   AdvertisingModel(image2: ImageAsset.advertingimage2,image1: ImageAsset.advertingimage1,phone: '+201559598288',whatsapp: 'https://www.facebook.com/Dr.MohamedZahar'),
   AdvertisingModel(image2: ImageAsset.advertingimage4,image1: ImageAsset.advertingimage3,phone: '+201019388578',whatsapp: 'https://www.facebook.com/profile.php?id=61550894865990'),
+  AdvertisingModel(image2: ImageAsset.advertingimage6,image1: ImageAsset.advertingimage5,phone: '+201283832896',whatsapp: 'https://www.facebook.com/profile.php?id=100090292286162'),
+
   //AdvertisingModel(image2: ImageAsset.advertingimage2,image1: ImageAsset.advertingimage1,phone: '+201211030844',whatsapp: 'Sekoo'),
 
 ];
--- lib/theme/advertisingimage.dart
+++ lib/theme/advertisingimage.dart
@@ -8,4 +8,6 @@ class ImageAsset{
   static const String advertingimage2='$rootimage/elzahar2.png';
   static const String advertingimage3='$rootimage/hossam.png';
   static const String advertingimage4='$rootimage/hossam2.png';
+  static const String advertingimage5='$rootimage/adel gweek.png';
+  static const String advertingimage6='$rootimage/adel gweek 2.png';
 }
